eee60f2d6d9625ae228b27e95eea7f65b9927cae
Martinelii <Kevenmartineli@outlook.com>
2024-11-28 13:32:55 -0300

    Efetuar Postagem

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -8,11 +8,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+
+import jakarta.validation.Valid;
 
 
 @RestController
@@ -30,13 +34,19 @@ public class PostagemController {
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<Postagem> getById(@PathVariable Long id){
-		return postagemRepository.findById(id).
-				map(resposta -> ResponseEntity.ok(resposta)).
-				orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+		return postagemRepository.findById(id). //recebendo um optional com objeto Postagem baseado no ID
+				map(resposta -> ResponseEntity.ok(resposta)). //Atribui Optional devolvido a resposta. Verifica se objeto foi encontrado.
+				orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());//Objeto não encontrado
 	}
 	
 	@GetMapping("/titulo/{titulo}")
 	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
-		return ResponseEntity.ok(postagemRepository.findByTituloContainingIgnoreCase(titulo));
+		return ResponseEntity.ok(postagemRepository.findByTituloContainingIgnoreCase(titulo)); //Retornando Status 200 se condição findByTitulo for true
+	}
+	
+	@PostMapping
+	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.CREATED) //Retorno status HTTP.
+				.body(postagemRepository.save(postagem)); //Adicionando ao corpo da requição a postagem
 	}
 }
